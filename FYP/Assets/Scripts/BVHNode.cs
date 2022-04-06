489fcba5212470452468f3ca323f9e45de058bdb
Kyle Sheridan <55580454+KyleSheridan@users.noreply.github.com>
2022-04-06 10:59:22 +0100

    Fixed BVHNode issues
    BVH constructor only takes in List<Node> and list is split instead of giving index's to constructor

--- FYP/Assets/Scripts/BVHNode.cs
+++ FYP/Assets/Scripts/BVHNode.cs
@@ -9,80 +9,141 @@ public class BVHNode : Node
     public Node left;
     public Node right;
 
-    int Box_Compare(Node a, Node b, int axis)
+    int BoxCompare(Node a, Node b, int axis)
     {
         return a.pos[axis].CompareTo(b.pos[axis]);
     }
 
-    int box_x_compare(Node a, Node b) {
-	    return Box_Compare(a, b, 0);
+    int BoxXCompare(Node a, Node b) {
+	    return BoxCompare(a, b, 0);
     }
 
-    int box_y_compare(Node a, Node b)
+    int BoxYCompare(Node a, Node b)
     {
-        return Box_Compare(a, b, 1);
+        return BoxCompare(a, b, 1);
     }
 
-    int box_z_compare(Node a, Node b)
+    int BoxZCompare(Node a, Node b)
     {
-        return Box_Compare(a, b, 2);
+        return BoxCompare(a, b, 2);
     }
 
-    BVHNode(List<Node> src_objects, int start, int end)
+    //BVHNode(List<Node> srcObjects, int start, int end)
+    //{
+    //    List<Node> objects = srcObjects;
+
+    //    int axis = UnityEngine.Random.Range(0, 3);
+
+    //    Comparison<Node> comparator = BoxXCompare;
+
+    //    switch (axis)
+    //    {
+    //        case 0:
+    //            comparator = BoxXCompare;
+    //            break;
+    //        case 1:
+    //            comparator = BoxYCompare;
+    //            break;
+    //        case 2:
+    //            comparator = BoxZCompare;
+    //            break;
+    //        default:
+    //            break;
+    //    }
+
+    //    int objectSpan = end - start;
+
+    //    if(objectSpan == 1)
+    //    {
+    //        left = right = objects[start];
+    //    }
+    //    else if(objectSpan == 2)
+    //    {
+    //        if(objects[start].pos[axis] < objects[start + 1].pos[axis])
+    //        {
+    //            left = objects[start];
+    //            right = objects[start + 1];
+    //        }
+    //        else
+    //        {
+    //            left = objects[start + 1];
+    //            right = objects[start];
+    //        }
+    //    }
+    //    else
+    //    {
+    //        // will not work as sorting whole list of nodes
+    //        objects.Sort(comparator);
+
+    //        int mid = start + objectSpan / 2;
+
+    //        left = new BVHNode(objects, start, mid);
+    //        right = new BVHNode(objects, mid, end);
+    //    }
+
+    //    int x = Mathf.Abs(objects[start].pos.x - objects[end - 1].pos.x);
+    //    int y = Mathf.Abs(objects[start].pos.y - objects[end - 1].pos.y);
+    //    int z = Mathf.Abs(objects[start].pos.z - objects[end - 1].pos.z);
+
+    //    pos = new Vector3Int(x, y, z);
+    //}
+    
+    BVHNode(List<Node> srcObjects)
     {
-        var objects = src_objects;
+        List<Node> objects = srcObjects;
 
         int axis = UnityEngine.Random.Range(0, 3);
 
-        Comparison<Node> comparator = box_x_compare;
+        Comparison<Node> comparator = BoxXCompare;
 
         switch (axis)
         {
             case 0:
-                comparator = box_x_compare;
+                comparator = BoxXCompare;
                 break;
             case 1:
-                comparator = box_y_compare;
+                comparator = BoxYCompare;
                 break;
             case 2:
-                comparator = box_z_compare;
+                comparator = BoxZCompare;
                 break;
             default:
                 break;
         }
 
-        int object_span = end - start;
+        int objectSpan = objects.Count;
 
-        if(object_span == 1)
+        if(objectSpan == 1)
         {
-            left = right = objects[start];
+            left = right = objects[0];
         }
-        else if(object_span == 2)
+        else if(objectSpan == 2)
         {
-            if(objects[start].pos[axis] < objects[start + 1].pos[axis])
+            if(objects[0].pos[axis] < objects[1].pos[axis])
             {
-                left = objects[start];
-                right = objects[start + 1];
+                left = objects[0];
+                right = objects[1];
             }
             else
             {
-                left = objects[start + 1];
-                right = objects[start];
+                left = objects[1];
+                right = objects[0];
             }
         }
         else
         {
+            // will not work as sorting whole list of nodes
             objects.Sort(comparator);
 
-            var mid = start + object_span / 2;
+            int mid = objectSpan / 2;
 
-            left = new BVHNode(objects, start, mid);
-            right = new BVHNode(objects, mid, end);
+            left = new BVHNode(objects.GetRange(0, mid));
+            right = new BVHNode(objects.GetRange(mid, objects.Count - mid));
         }
 
-        int x = Mathf.Abs(objects[start].pos.x - objects[end].pos.x);
-        int y = Mathf.Abs(objects[start].pos.y - objects[end].pos.y);
-        int z = Mathf.Abs(objects[start].pos.z - objects[end].pos.z);
+        int x = Mathf.Abs(objects[0].pos.x - objects[objects.Count - 1].pos.x);
+        int y = Mathf.Abs(objects[0].pos.y - objects[objects.Count - 1].pos.y);
+        int z = Mathf.Abs(objects[0].pos.z - objects[objects.Count - 1].pos.z);
 
         pos = new Vector3Int(x, y, z);
     }
